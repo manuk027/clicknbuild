2cb462a1f6087a2505d6b11e582e71d337f83b02
Manu Krishna <krishnamanu27112004@gmail.com>
2025-10-11 10:35:32 +0530

    Connected Database

--- app.js
+++ app.js
@@ -1,13 +1,15 @@
 import express from "express";
 import dotenv from "dotenv";
+import { connectDB } from "./config/db.js";
 
 dotenv.config();
+connectDB();
 const app = express();
 
-app.get('/', (req, res)=>{
+app.get('/', (req, res) => {
     res.send("Sample server");
 })
 
-app.listen(process.env.PORT, ()=>{
+app.listen(process.env.PORT, () => {
     console.log("http://localhost:3000");
 })
---
+++ config/db.js
@@ -0,0 +1,15 @@
+import mongoose from "mongoose";
+import dotenv from "dotenv";
+
+dotenv.config();
+
+export const connectDB = async () => {
+    try {
+        await mongoose.connect(process.env.MONGODB_URI);
+        console.log("Mongodb connected");
+    } catch (err) {
+        console.log(err.message);
+        process.exit(1);
+    }
+}
+
